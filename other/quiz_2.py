49808191c1fbb40dc4f7891cf57a3608c26c31e8
Forrest Smith <fasmith0229@gmail.com>
2022-07-02 07:57:47 -0500

    updated list comprehensions

---
+++ other/quiz_2.py
@@ -0,0 +1,61 @@
+# Quiz 2
+
+# This variable will be used throughout the quiz
+weather = [
+    ('USW00094889', '2014-03-08', 'TMIN', -71),
+    ('USW00094889', '2014-03-08', 'TMAX', 33),    
+    ('USW00094889', '2014-03-09', 'TMAX', 17),
+    ('USW00094889', '2014-03-09', 'TMIN', -116),  
+    ('USW00094889', '2014-09-03', 'TMAX', 278),
+    ('USW00094889', '2014-09-03', 'TMIN', 106),   
+    ('USW00094889', '2014-09-04', 'TMIN', 111),
+    ('USW00094889', '2014-09-04', 'TMAX', 306),   
+]
+# Q1. The variable first_date contains the date string
+#     of the first element of weather with format
+#     yyyy-mm-dd.
+#
+#     Convert the month number to an int and assign
+#     the value to the variable month.
+first_date = weather[0][1]
+# month = ?
+
+# Q2. Create a new list from weather containing all
+#     records with a 'TMIN' element. 
+#     Name this list tmin.
+
+# tmin = ? 
+
+
+
+# Q3. Create a new list containing all records with
+#     a month of 9.
+#     Name this list september.
+
+# september = ?
+
+
+# Q4. Remove the extension from the variable filename
+#     (with code) and save it to a variable named
+#     basename
+filename = 'cat_pajamas.gif'
+# basename = ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
